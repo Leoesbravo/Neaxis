f760fdce69cbb8b9d523618db13d057e9bcbfd19
Leoesbravo <lescogidobravo@outlook.com>
2022-12-13 20:00:06 -0600

    Add project files.

---
+++ PruebaNeaxis/Controllers/PruebaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PruebaNeaxis.Controllers
+{
+    public class PruebaController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
